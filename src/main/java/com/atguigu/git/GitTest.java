1b9c870834d220b02b8b3c37e32fedb6d3ca2107
qiwk <qiwk916@163.com>
2021-11-26 14:54:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author qiwk
+ * @creat 2021-11-26 14:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
